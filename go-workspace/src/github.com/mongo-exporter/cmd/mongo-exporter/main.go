7579bf8cac1a3ddf1966624f3ce7281262246537
Mike Sallese <msallese31@gmail.com>
2018-04-10 20:23:23 -0400

    change endpoint to /export; return json

--- go-workspace/src/github.com/mongo-exporter/cmd/mongo-exporter/main.go
+++ go-workspace/src/github.com/mongo-exporter/cmd/mongo-exporter/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os/exec"
@@ -11,7 +12,7 @@ import (
 func main() {
 	fmt.Println("Starting server")
 	// TODO: Chain handlers and have a logging handler
-	http.Handle("/", exportHandler{})
+	http.Handle("/export", exportHandler{})
 	http.ListenAndServe(":8080", nil)
 }
 
@@ -19,7 +20,7 @@ type exportHandler struct{}
 
 func (exportHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Running export")
-	runMongoExport()
+	runMongoExport(w)
 	runls()
 }
 
@@ -32,7 +33,7 @@ func runls() {
 	fmt.Println(string(cmd))
 }
 
-func runMongoExport() {
+func runMongoExport(w http.ResponseWriter) {
 	t := time.Now()
 	timestamp := t.Format("01-02-06-3:04PM")
 	commandString := fmt.Sprintf("mongoexport -d test -c accelData -u main_admin -p abc123 --authenticationDatabase admin --jsonArray --host mongodb-service:27017 > %s", timestamp)
@@ -42,13 +43,26 @@ func runMongoExport() {
 	var stderr bytes.Buffer
 	cmd.Stdout = &out
 	cmd.Stderr = &stderr
+	respJSON := Response{}
+
 	err := cmd.Run()
 	if err != nil {
 		fmt.Println("error!")
 		fmt.Println(err.Error())
 		fmt.Println(stderr.String())
-
+		respJSON.Status = err.Error()
+	} else {
+		statusString := fmt.Sprintf("Success! %s", timestamp)
+		respJSON.Status = statusString
 	}
 	// fmt.Println(stderr.String())
 	fmt.Println(out.String())
+	json.NewEncoder(w).Encode(respJSON)
+}
+
+// DBCountResponse is the json data type we respond to for Android that tells how many entries in the AccelData collection
+type Response struct {
+	// The `json` struct tag maps between the json name
+	// and actual name of the field
+	Status string `json:"status"`
 }
